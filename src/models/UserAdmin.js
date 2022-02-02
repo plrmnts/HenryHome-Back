0211277ac084bae0556387ad7e12c0e86c8ae81b
Santibio <87582357+Santibio@users.noreply.github.com>
2022-02-02 12:07:28 -0300

    feat - userAdmin and location models

---
+++ src/models/UserAdmin.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  sequelize.define(
+    "userAdmin",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      rol: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaulValue: "Admin",
+      }
+    },
+    { timestamps: false }
+  );
+};
