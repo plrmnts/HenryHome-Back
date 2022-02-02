e00c564e0be1cdccf570184dd8bf930b8091a44a
Josue Lamas <itzjosue99@gmail.com>
2022-02-02 16:33:27 -0500

    routes added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,15 @@
 const { Router } = require("express");
 const user = require('./user.route.js')
 const house = require('./house.route.js')
-<<<<<<< HEAD
 const facilities = require('./facilities.route')
+const servicies = require('./servicies.route')
 
-=======
-const servicie = require('./servicies.route.js')
->>>>>>> ba7a101f70a6b0e65bacd6acb5aa39dd05c4b33c
 const router = Router()
-
 
 router.use("/user",user)
 router.use("/houses",house)
-<<<<<<< HEAD
 router.use('/facilities',facilities)
-=======
-router.use("/servicie",servicie)
-
->>>>>>> ba7a101f70a6b0e65bacd6acb5aa39dd05c4b33c
-
+router.use('/services',servicies)
 
 
 module.exports = router
