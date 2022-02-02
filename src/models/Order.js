e316087b93a73d32b43f1c11847ba94d1c251384
Santibio <87582357+Santibio@users.noreply.github.com>
2022-02-02 14:51:26 -0300

    feat - model order

---
+++ src/models/Order.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Order",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      amount: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaulValue: "Pending",
+      },
+    },
+    { timestamps: true }
+  );
+};
