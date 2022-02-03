8f1ce60e40df8282db3a4db1c1bc61457e5d684c
Josue Lamas <itzjosue99@gmail.com>
2022-02-03 14:58:53 -0500

    routes services adn facilities changed

--- src/controllers/facilities.controller.js
+++ src/controllers/facilities.controller.js
@@ -4,28 +4,28 @@ const { facilitiesArray } = require('../config_db/facilities.array')
 const createFacilitie = async (req,res)=>{
     const { name } = req.body
     try{
-        const arr = []
-        facilitiesArray.forEach(async (f)=>{
-            const newFacilitie = await Facilities.findOrCreate({
-                where:{
-                    name:f.toLowerCase()
-                }
-            })    
-            arr.push(newFacilitie)
-        })
-
-        
         if(name){
-            const facilitie = await Facilities.findOrCreate({where:{name: name.toLowerCase()}})    
-        
-            res.status(201).json(facilitie)}
+            const facility = await Facilities.findOrCreate({where:{name: name.toLowerCase()}}) 
+            res.status(201).json(facility)
+        }else{
+            res.status(400).json({mesagge:"Data needed."})
+        }
     }catch(err){
         console.log(err)
+        res.status(500).json(err)
     }
 }
 
 const getFacilities = async (req,res)=>{
     try{
+        facilitiesArray.forEach(async (f)=>{
+            await Facilities.findOrCreate({
+                where:{
+                    name:f.toLowerCase()
+                }
+            })    
+        })
+
         const facilities = await Facilities.findAll()
         res.status(200).json(facilities)
     }catch(err){
--- src/controllers/services.controller.js
+++ src/controllers/services.controller.js
@@ -4,33 +4,30 @@ const { servicesArray } = require("../config_db/services.array.js");
 const createService = async (req, res) => {
   const { name } = req.body;
   try {
-    const arr = [];
-
-    servicesArray.forEach(async (e) => {
-      const newServ = await Services.findOrCreate({
-        where: {
-          name: e.toLowerCase(),
-        },
-      });
-      arr.push(newServ);
-    });
-
     if (name) {
       const service = await Services.findOrCreate({
         where: { name: name.toLowerCase() },
       });
-
-      res.status(201).json(service);
+      res.json(service);
     } else {
-      res.status(201).json(arr);
+      res.status(400).json(arr);
     }
   } catch (err) {
     console.log(err);
+    res.status(500).json(err)
   }
 };
 
 const getServices = async (req, res) => {
   try {
+    servicesArray.forEach(async (e) => {
+      await Services.findOrCreate({
+        where: {
+          name: e.toLowerCase(),
+        },
+      });
+    });
+
     const services = await Services.findAll();
     res.json(services);
   } catch (err) {
