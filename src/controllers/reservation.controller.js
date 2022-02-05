05d040a0d2782c3630fd1924b058235b7a2b13d3
Josue Lamas <itzjosue99@gmail.com>
2022-02-04 19:27:58 -0500

    reservation route added

--- src/controllers/reservation.controller.js
+++ src/controllers/reservation.controller.js
@@ -2,10 +2,10 @@ const { Reservations } = require('../db')
 
 const createReservation = async(req,res)=>{
     
-    const { id_hotel, id_client, date_start, id_order, date_end, details } = req.body
+    const { id_hotel, id_client, date_start, id_order, date_end, detail } = req.body
     // Suponiendo que son los unicos datos que llegan por body
     try{
-        if(!id_hotel || !id_client || !date_start || !date_end || details || !id_order){
+        if(!id_hotel || !id_client || !date_start || !date_end || !detail || !id_order){
             return res.json(400).json({msg: "Data needed!"})
         }
         const newReservation = await Reservations.create(req.body)
@@ -13,10 +13,25 @@ const createReservation = async(req,res)=>{
     }catch(err){
         res.status(500).json(err)
     }
+}
 
-
+const updateReservation = async (req,res)=>{
+    const { id } = req.body
+    try{
+        await Reservations.update({
+            status:'Resolved'
+        },{
+            where:{
+                id:id
+            }
+        })
+        res.status(200).json({msg:"Reservation updated",})
+    }catch(err){
+        res.status(500).json(err)
+    }
 }
 
 module.exports = {
-    createReservation
+    createReservation,
+    updateReservation
 }
--- src/routes/reservation.route.js
+++ src/routes/reservation.route.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
-const { createReservation } = require('../controllers/reservation.controller')
+const { createReservation , updateReservation } = require('../controllers/reservation.controller')
 
 router.post('/',createReservation)
+router.put('/',updateReservation)
 
 module.exports = router
